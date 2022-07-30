18a9ad7980c4ae508f7a638c878e626e8e198858
reindra-ubuntu <reindrairawan@gmail.com>
2022-07-30 23:04:19 +0700

    fitur login

---
+++ app/src/main/java/com/example/pendaftaran/login/LoginActivity.java
@@ -0,0 +1,63 @@
+package com.example.pendaftaran.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.pendaftaran.MainActivity;
+import com.example.pendaftaran.R;
+import com.google.android.material.animation.DrawableAlphaProperty;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        setStatusBarGradiant(LoginActivity.this);
+        getSupportActionBar().hide();
+        login = findViewById(R.id.tvlogin);
+
+        login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tvlogin:
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        }
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public void setStatusBarGradiant(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = activity.getWindow();
+            Drawable background = activity.getResources().getDrawable(R.drawable.gradient);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(activity.getResources().getColor(android.R.color.transparent));
+            window.setNavigationBarColor(activity.getResources().getColor(android.R.color.transparent));
+            window.setBackgroundDrawable(background);
+
+        }
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
+}
