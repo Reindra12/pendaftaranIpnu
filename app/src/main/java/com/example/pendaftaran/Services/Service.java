2c5edec6ca0531bb3059c8863ae0347b503bf021
reindra-ubuntu <reindrairawan@gmail.com>
2022-08-15 16:24:07 +0700

    penambahan path foto

--- app/src/main/java/com/example/pendaftaran/Services/Service.java
+++ app/src/main/java/com/example/pendaftaran/Services/Service.java
@@ -2,7 +2,9 @@ package com.example.pendaftaran.Services;
 
 public class Service {
     public static final String URL = "http://192.168.227.43/administrasi_ku/Api/";
-    public static final String gambarsurat = "http://192.168.227.43/administrasi_ku/assets/upload/surat_keluar/";
+    public static final String URLgambar = "http://192.168.227.43/administrasi_ku/assets/upload/";
+    public static final String gambarsurat = "surat_keluar/";
+    public static final String gambarkegiatan = "kegiatan/";
     public static final String regis = "register/";
     public static final String login = "login/";
     public static final String spinsurat = "spindatasurat/";
